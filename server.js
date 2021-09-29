63b18a3996fda537051779a2c7ccc181031b6197
Lucas Aebi <lucas.aebi@gmail.com>
2021-09-29 11:36:27 +0300

    moved server related code to server.js

---
+++ server.js
@@ -0,0 +1,40 @@
+const express =  require('express')
+const app = express()
+const passport = require('passport')
+
+
+// Set up global data state
+var data = require('./data')
+
+// Setup passport
+require('./authentication').setup(passport, data)
+
+
+// json parsing middleware (bodyparser is deprecated)
+app.use(express.urlencoded({extended: true}));
+app.use(express.json()) // To parse the incoming requests with JSON payloads
+
+// Import routes
+let usersRoutes = require('./routes/usersRouter')(passport, data)
+let itemsRoutes = require('./routes/itemsRouter')(passport, data)
+
+// use imported routes
+app.use('/users', usersRoutes)
+app.use('/items', itemsRoutes)
+
+
+const port = 3000
+// launch app
+
+let serverInstance = null
+
+module.exports = {
+  start: () => {
+    serverInstance = app.listen(port, () => {
+      console.log(`Example app listening at http://localhost:${port}`)
+    })
+  },
+  close: () => {
+    server.close()
+  }
+}
